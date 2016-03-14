dcc7240a6c1489652cdd04f97b52dbdb5a0270dd
kaio77 <kaio77@hawaii.edu>
2016-03-13 21:07:39 -1000

    Added files via upload

--- AlohaTourGuide/src/com/paliokimotoramano/alohatourguide/OahuEvent.java
+++ AlohaTourGuide/src/com/paliokimotoramano/alohatourguide/OahuEvent.java
@@ -23,11 +23,26 @@ public class OahuEvent implements Serializable {
 	// This method is used to create the list of events that will be cycled on the Explore Page
 	// NEED TO IMPLEMENT
 	public static ArrayList<OahuEvent> createEvents() {
-		ArrayList<OahuEvent> events = new ArrayList<OahuEvent>();
 		OahuEvent waimeabw = new OahuEvent(R.drawable.waimea, "    Jump off Waimea Rock    ", "Located at Waimea Bay, go jump off the Rock!");
 		OahuEvent giovannis = new OahuEvent(R.drawable.giovannis, "   Giovanni's Shrimp Truck  ", "Located on the North Shore, go get some shrimp!");
+		OahuEvent maksLighthouse = new OahuEvent(R.drawable.makslighthouse, "   Makapu'u Lighthouse Trail   ", "Located on the east side of Oahu, enjoy the view!");
+		OahuEvent diamondHeadHike = new OahuEvent(R.drawable.diamondheadhike, "   Diamond Head Hike Trail    ", "Located east of Waikiki, enjoy the view!");
+		OahuEvent spittingCaves = new OahuEvent (R.drawable.spittingcaves, "   Spitting Caves Cliffs   ", "Located on the east side of Oahu, enjoy the view!");
+		OahuEvent hanaaumaBay = new OahuEvent (R.drawable.hanaaumabay, "   Hana'ama Bay   ", "Located on the east side of oahu, swim with the fishes!");
+		OahuEvent kokoHead = new OahuEvent (R.drawable.kokohead, "    Koko Head Hike    ", "Located on the east side of Oahu, reward yourself with the view!");
+		OahuEvent maunawili = new OahuEvent (R.drawable.maunawili, "    Maunawili Waterfall Hike   ", "Enjoy the hike and be rewarded with a beautiful waterfall!");
+		OahuEvent stairway = new OahuEvent (R.drawable.stairway, "    Stairway to Heaven Hike   ", "Located on the east side of Oahu, challenge yourself and enjoy the view!");
+		OahuEvent yokohama = new OahuEvent (R.drawable.yokohama, "    Yokohama Beach    ", "Located on the west side of Oahu, Enjoy the the beautiful ocean!");
 		events.add(waimeabw);
 		events.add(giovannis);
+		events.add(maksLighthouse);
+		events.add(diamondHeadHike);
+		events.add(spittingCaves);
+		events.add(hanaaumaBay);
+		events.add(kokoHead);
+		events.add(maunawili);
+		events.add(stairway);
+		events.add(yokohama);
 		return events;
 	}
 	
